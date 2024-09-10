6f3d3640b6f80cdcb057477342e63395d8f8903f
Geon-You <yjg5904@gmail.com>
2024-09-10 14:24:15 +0900

    First Commit

---
+++ BillBoard.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Include.h"
+
+class BillBoard
+{
+private:
+	LPDIRECT3DTEXTURE9 tex;
+	IDirect3DVertexBuffer9* vb;
+
+public:
+	void Initialize();
+	void Update();
+	void Render();
+	void Release();
+};
